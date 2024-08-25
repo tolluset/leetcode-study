54fb6a3ae0a897adbd3094a068b100cd52975223
tolluset <dlqud19@gmail.com>
2024-08-26 04:52:28 +0900

    solve: two sum

---
+++ two-sum/tolluset.ts
@@ -0,0 +1,22 @@
+/*
+ * TC: O(n)
+ * SC: O(n)
+ * */
+function twoSum(nums: number[], target: number): number[] {
+  const n = nums.length;
+  const answer = new Map<number, number>();
+
+  for (let i = 0; i < n; i++) {
+    const diff = target - nums[i];
+
+    const before = answer.get(diff);
+
+    if (before !== undefined) {
+      return [before, i];
+    }
+
+    answer.set(nums[i], i);
+  }
+
+  return [];
+}
